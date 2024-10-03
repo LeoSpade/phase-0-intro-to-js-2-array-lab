d29a8319d2f073f852e55e3225f1f04bd6acee0f
thornm <atmaystar@gmail.com>
2024-10-03 12:46:14 -0400

    commit array lab1

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = [
+    "Milo", "Otis", "Garfield",
+    ];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newCatsArray = [...cats, "Broom"];
+    return newCatsArray;
+}
+
+function prependCat(name) {
+    const newCatsArray = ["Arnold", ...cats];
+    return newCatsArray;
+}
+
+function removeLastCat() {
+    const newCatsArray = cats.slice(0, -1);
+    return newCatsArray;
+}
+
+function removeFirstCat() {
+    const newCatsArray = cats.slice(1);
+    return newCatsArray;
+}
